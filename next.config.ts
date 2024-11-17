5346f3300e59bfa85a3f9eccf3d08139380d46d8
shayan shirazian <shayan@colaskitchen.com>
2024-11-17 13:34:03 +0330

    default files modified

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+const nextConfig = {
+    // other configurations
 };
 
-export default nextConfig;
+module.exports = nextConfig;
