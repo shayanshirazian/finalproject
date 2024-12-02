6b93941b897e76ab80f284d9d507cd4a63ff5bc5
shayan shirazian <shayan@colaskitchen.com>
2024-12-02 09:01:42 +0330

    codes fixed v0.2

--- app/profile/components/buttons/CancelBtn.jsx
+++ app/profile/components/buttons/CancelBtn.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
-const CancelBtn = ({onClick}) => {
+const CancelBtn = () => {
     return (
         <button
             className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md hover:bg-gray-400"
-            onClick={onClick}
-        >
-            Cancel
+            onClick={onCancel}>
+            {`Cancel`}
         </button>
     );
 };
