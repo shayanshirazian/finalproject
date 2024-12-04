2c1eb80ae1e4f67d8f3be5188aa16c79b0e5732a
shayan shirazian <shayan@colaskitchen.com>
2024-12-04 11:41:33 +0330

    Ibel order details shown in detail cart data is not dynamic params fixed

--- app/profile/components/modals/ContentModal.jsx
+++ app/profile/components/modals/ContentModal.jsx
@@ -5,14 +5,50 @@ import SecondaryRow from "../cartDetails/SecondaryRow";
 import TertiaryRow from "../cartDetails/TertiaryRow";
 import QuaternaryRow from "../cartDetails/QuaternaryRow";
 
-const FutureOrderDetails = () => (
-    <div className="overflow-y-auto max-h-[90vh] p-3">
-        <div><PrimaryRow/></div>
-        <div><SecondaryRow/></div>
-        <Divider/>
-        <div><TertiaryRow/></div>
-        <Divider/>
-        <div><QuaternaryRow /></div>
-    < /div>
-)
+const FutureOrderDetails = () => {
+        // Sample orders array (could be dynamic or fetched)
+        const orders = [
+                {
+                        dogName: "Ibel",
+                        status: "Shipped",
+                        portions: "Full meal",
+                        recipes: "2 Chicken, 4 Beef",
+                        deliveryDate: "2024-09-04",
+                },
+        ];
+
+        // Repeat the same structure 3 times by mapping over [1, 2, 3]
+        return (
+            <div className="overflow-y-auto max-h-[90vh] p-3">
+                    {[1, 2, 3].map((_, index) => (
+                        <div key={index} className="border p-4 mb-4 rounded-lg shadow-md">
+                                <div>
+                                        <PrimaryRow dogName={orders[0].dogName} status={orders[0].status} />
+                                </div>
+                                <Divider />
+                                <div>
+                                        <SecondaryRow title="Portions" value={orders[0].portions} />
+                                </div>
+                                <Divider />
+                                <div>
+                                        <TertiaryRow title="Recipes" value={orders[0].recipes} />
+                                </div>
+                                <Divider />
+                                <div>
+                                        <QuaternaryRow
+                                            title="Delivery Date"
+                                            value={orders[0].deliveryDate}
+                                            order={null}
+                                            onOpenModal={null}
+                                            isEditing={null}
+                                            newValue={null}
+                                            setNewValue={null}
+                                        />
+                                </div>
+                        </div>
+                    ))}
+            </div>
+        );
+};
+
 export default FutureOrderDetails;
