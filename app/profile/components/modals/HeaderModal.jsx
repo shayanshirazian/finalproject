5a2109263b31ffa1ad264ff3d31d62095ab98c2a
shayan shirazian <shayan@colaskitchen.com>
2024-12-04 09:21:39 +0330

    Header Close btn fixed

--- app/profile/components/modals/HeaderModal.jsx
+++ app/profile/components/modals/HeaderModal.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import CancelBtn from "../buttons/CancelBtn";
+import CloseBtn from "../buttons/CloseBtn";
 
 const HeaderModal = ({title, onClose}) => (
     <div className="flex justify-between items-center mb-3">
         <span className="text-lg font-bold text-gray-800">{title}</span>
-        <CancelBtn onClose={onClose}/>
+        <CloseBtn onClose={onClose}/>
     </div>
 )
 export default HeaderModal;
