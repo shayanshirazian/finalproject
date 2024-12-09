72c7d822738a2a85fe5f9a5730d650de3d1dd557
shayan shirazian <shayan@colaskitchen.com>
2024-12-09 13:09:24 +0330

    EditDate functionality fixed

--- app/profile/components/modals/OrderDetailsModal.jsx
+++ app/profile/components/modals/OrderDetailsModal.jsx
@@ -1,46 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HeaderModal from "./HeaderModal";
-import ContentModal from "./ContentModal";
-// import ActionsModal from "./ActionsModal";
+import ActionsModal from "./ActionsModal";
 import Divider from "../cartDetails/Divider";
 
-const OrderDetailsModal = ({
-  isOpen,
-  onClose,
-  orderDetails,
-  //    onSave,
-  onOpenModal,
-}) => {
-  // const [isEditing, setIsEditing] = useState(false);
-  // const [newDate, setNewDate] = useState(orderDetails?.deliveryDate || "");
+const OrderDetailsModal = ({ isOpen, onClose, orderDetails, onSave }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newDate, setNewDate] = useState(orderDetails?.deliveryDate || "");
+
+  useEffect(() => {
+    if (orderDetails) {
+      setNewDate(orderDetails.deliveryDate || "");
+    }
+  }, [orderDetails]);
 
   if (!isOpen || !orderDetails) return null;
 
-  // const handleSave = () => {
-  //     if (!newDate) return;
-  //     onSave(orderDetails.dogName, newDate);
-  //     setIsEditing(false);
-  // };
-  // const handleCancel = () => {
-  //     setIsEditing(false);
-  //     setNewDate(orderDetails.deliveryDate);
-  // };
+  const handleSave = () => {
+    if (!newDate) return;
+    onSave(orderDetails.dogName, newDate);
+    setIsEditing(false);
+  };
+
+  const handleCancel = () => {
+    setIsEditing(false);
+    setNewDate(orderDetails.deliveryDate);
+  };
 
   return (
     <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center">
-      {/* Backdrop for the modal with dark semi-transparent background. */}
       <div className="bg-white rounded-xxl shadow-lg w-3/4 max-w-lg p-3 relative">
-        {/* Modal container with white background, rounded corners, and shadow. */}
         <HeaderModal title="Order Details" onClose={onClose} />
         <Divider />
-        <ContentModal order={orderDetails} onOpenModal={onOpenModal} />
+        <div className="DateEditing p-4">
+          {isEditing ? (
+            <div>
+              <label className=" text-sm font-medium text-gray-700">
+                Delivery Date:
+              </label>
+              <input
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                type="date"
+                value={newDate}
+                onChange={(e) => setNewDate(e.target.value)}
+              />
+            </div>
+          ) : (
+            <div className=" flex justify-between mx-3 text-lg font-semibold ">
+              <span className="text-gray-500">Delivery Date:</span>
+              <span className="text-green-700">
+                {orderDetails.deliveryDate}
+              </span>
+            </div>
+          )}
+        </div>
 
-        {/*<ActionsModal*/}
-        {/*    isEditing={isEditing}*/}
-        {/*    onSave={handleSave}*/}
-        {/*    onCancel={handleCancel}*/}
-        {/*    onEdit={() => setIsEditing(true)}*/}
-        {/*/>*/}
+        <ActionsModal
+          isEditing={isEditing}
+          onSave={handleSave}
+          onCancel={handleCancel}
+          onEdit={() => setIsEditing(true)}
+        />
       </div>
     </div>
   );
